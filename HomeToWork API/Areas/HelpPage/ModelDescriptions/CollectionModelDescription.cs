3e195fdb99d9a33093be08db3f04fcfe4f976abf
Federico Mastrini <mastro993@users.noreply.github.com>
2018-03-03 11:09:26 +0100

    Aggiungere i file di progetto.

---
+++ HomeToWork API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HomeToWork_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ HomeToWork API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HomeToWork_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
